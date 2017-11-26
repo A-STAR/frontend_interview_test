3aff507d593884d41eb42bbd9172edbcc10414f2
A-STAR <andreysuperstar@gmail.com>
2017-11-26 18:25:42 +0300

    Add API to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.mcmakler.de',
+  version: 1,
+  api: `${this.url}/v${this.version}`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.mcmakler.de',
+  version: 1,
+  api: `${this.url}/v${this.version}`
 };
